19595fe5ce5aa09cbe697d1af84df9385847678e
Ryo Fuji <ryo176999@gmail.com>
2022-12-15 19:51:58 -0800

    Initial Commit

---
+++ AIChat-OpenAI/AIChat_OpenAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChat_OpenAIApp.swift
+//  AIChat-OpenAI
+//
+//  Created by ryofuji on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AIChat_OpenAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
