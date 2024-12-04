3dbd3aebd5028e99a7af41ffca2b91a9318e788a
Ryo Fujimura <ryo176999@gmail.com>
2024-12-03 17:02:42 -0800

    image imported from pervious project

--- With-OpenAI/ContentView.swift
+++ With-OpenAI/ContentView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            ImageView()
+        ScrollView{
+            VStack {
+                ImageView()
+            }
         }
-        .padding()
     }
 }
 
