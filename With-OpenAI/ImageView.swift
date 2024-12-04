e2c5c8c265c5b0f6d2accce4d6aeb3f78607c364
Ryo Fujimura <ryo176999@gmail.com>
2024-12-03 16:43:08 -0800

    installed openaikit package

---
+++ With-OpenAI/ImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageView.swift
+//  With-OpenAI
+//
+//  Created by ryo fujimura on 2024/12/3.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageView()
+}
