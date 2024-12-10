c8b58a85d291120c34d0b0c38b753e0a12f7b79b
Ryo Fujimura <ryo176999@gmail.com>
2024-12-10 14:18:35 -0800

    Groq init

---
+++ With-OpenAI/HeartWarningGroqModel.swift
@@ -0,0 +1,11 @@
+//
+//  HeartWarningGroqModel.swift
+//  With-OpenAI
+//
+//  Created by ryo fujimura on 2024/12/10.
+//
+
+import Foundation
+
+class HeartWarmingGroqModel{
+}
---
+++ With-OpenAI/HeartWarningGroqView.swift
@@ -0,0 +1,18 @@
+//
+//  HeartWarningGroqView.swift
+//  With-OpenAI
+//
+//  Created by ryo fujimura on 2024/12/10.
+//
+
+import SwiftUI
+
+struct HeartWarningGroqView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeartWarningGroqView()
+}
