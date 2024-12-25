37022b3cff02828553c23d52b069abf5fb5cea93
Ryo Fujimura <ryo176999@gmail.com>
2024-12-24 18:21:09 -0800

    localllm initi reset

---
+++ With-OpenAI/LocalLLMView.swift
@@ -0,0 +1,18 @@
+//
+//  LocalLLMView.swift
+//  With-OpenAI
+//
+//  Created by ryo fujimura on 2024/12/23.
+//
+
+import SwiftUI
+
+struct LocalLLMView: View {
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+#Preview {
+    LocalLLMView()
+}
