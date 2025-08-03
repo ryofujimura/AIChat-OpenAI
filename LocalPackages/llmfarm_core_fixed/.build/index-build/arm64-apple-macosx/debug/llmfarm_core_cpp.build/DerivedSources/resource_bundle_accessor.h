4d95da894216b5724800581547b1d31291597117
Ryo Fujimura <ryo176999@gmail.com>
2025-08-03 15:41:11 -0400

    local model

---
+++ LocalPackages/llmfarm_core_fixed/.build/index-build/arm64-apple-macosx/debug/llmfarm_core_cpp.build/DerivedSources/resource_bundle_accessor.h
@@ -0,0 +1,15 @@
+#if __OBJC__
+#import <Foundation/Foundation.h>
+
+#if __cplusplus
+extern "C" {
+#endif
+
+NSBundle* llmfarm_core_cpp_SWIFTPM_MODULE_BUNDLE(void);
+
+#define SWIFTPM_MODULE_BUNDLE llmfarm_core_cpp_SWIFTPM_MODULE_BUNDLE()
+
+#if __cplusplus
+}
+#endif
+#endif
